634f377e6387e8371356c3b587141a5157657c48
wumo1016 <760478279@qq.com>
2020-10-14 17:40:25 +0800

    optimize

--- qiankun_vue1/src/main.js
+++ qiankun_vue1/src/main.js
@@ -10,18 +10,15 @@ function render(props) {
     instance = new Vue({
         router,
         render: h => h(App)
-    }).$mount('#app'); // 这里是挂载到自己的html中  基座会拿到这个挂载后的html 将其插入进去
+    }).$mount('#app');
 }
 
-if (window.__POWERED_BY_QIANKUN__) { // 动态添加publicPath
+if (window.__POWERED_BY_QIANKUN__) {
     __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
-}
-
-if (!window.__POWERED_BY_QIANKUN__) { // 默认独立运行
+} else {
     render();
 }
 
-// 子组件的协议就ok了
 export async function bootstrap(props) {
 
 };
